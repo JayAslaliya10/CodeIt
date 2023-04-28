bca0bf0d79a1cbd3d61dbb1e1a2c9eac1bebe32f
Rashid Aziz <md.r.a.n.786@gmail.com>
2023-04-28 16:24:33 +0530

    added web comoiler

--- src/components/WebIndex.js
+++ src/components/WebIndex.js
@@ -1,70 +1,70 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import WebEditor from './WebEditor'
-import useLocalStorage from '../hooks/useLocalStorage'
-import '../css/webeditor.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIconName } from '@fortawesome/free-solid-svg-icons';
+import "../css/webeditor.css"
 
-function WebIndex() {
-  const [html, setHtml] = useLocalStorage('html', '')
-  const [css, setCss] = useLocalStorage('css', '')
-  const [js, setJs] = useLocalStorage('js', '')
+function App() {
   const [srcDoc, setSrcDoc] = useState('')
-  const f1 = (val) => {
-    setHtml(val);
-  }
-  const f2 = (val) => {
-    setCss(val);
-  }
-  const f3 = (val) => {
-    setJs(val);
-  }
+  const [html, setHtml] = useState("<h1>Weclome to Codeit</h1>")
+  const [css, setCss] = useState("h1{\n\tcolor:red\n}");
+  const [js, setJs] = useState("// type your Javascript here...");
   useEffect(() => {
     const timeout = setTimeout(() => {
-      console.log("inside ", html, css, js);
       setSrcDoc(`
         <html>
-          <body>${html}</body>
-          <style>${css}</style>
-          <script>${js}</script>
+        <style>${css}</style>
+        <body>${html}</body>
+        <script>${js}</script>
         </html>
       `)
     }, 250)
     return () => clearTimeout(timeout)
   }, [html, css, js])
-
   return (
     <>
-      <div className="pane top-pane">
-        <WebEditor
-          language="xml"
-          displayName="HTML"
-          value={html}
-          onChange={f1}
-        />
-        <WebEditor
-          language="css"
-          displayName="CSS"
-          value={css}
-          onChange={f2}
-        />
-        <WebEditor
-          language="javascript"
-          displayName="JS"
-          value={js}
-          onChange={f3}
-        />
-      </div>
-      <div className="pane">
-        <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-        />
+      <div className='web-container'> 
+        <div className='left-cont'>
+          <div className="ide-cont html-cont">
+            <WebEditor
+              language="html"
+              displayName="HTML"
+              value={html}
+              onChange={setHtml}
+              />
+          </div>
+          <div className="ide-cont css-cont">
+            <WebEditor
+              language="css"
+              displayName="CSS"
+              value={css}
+              onChange={setCss}
+              />
+          </div>
+          <div className="ide-cont js-cont">
+            <WebEditor
+              language="javascript"
+              displayName="JAVASCRIPT"
+              value={js}
+              onChange={setJs}
+              />
+          </div>
+        </div>
+        <div className="pane">
+          <h2 className='pane-heading'>Web Compiler</h2>
+          <iframe
+            className='dev-output'
+            srcDoc={srcDoc}
+            title="output"
+            sandbox="allow-scripts"
+            frameBorder="0"
+            width="100%"
+            height="100%"
+            />
+        </div>
       </div>
     </>
   )
 }
 
-export default WebIndex;
+export default App;
